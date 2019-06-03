9286c824fd277343c7885f6c3caed6ee5a486716
Administrator <Administrator@PC-2019HWL>
2019-06-03 16:44:14 +0800

    add

---
+++ mytask-controller/src/main/java/com/qjxs/aspect/BaseRunnable.java
@@ -0,0 +1,20 @@
+package com.qjxs.aspect;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author lidaye
+ *
+ */
+public abstract class BaseRunnable implements Runnable{
+
+	/**
+	 * 被循环执行的次数
+	 */
+	public AtomicInteger loopCount = new AtomicInteger();
+	
+	
+	
+	
+	
+}
